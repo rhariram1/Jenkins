26c46f69291a988d53fb6c09e350b0679c8aba34
rhariram1 <64982850+rhariram1@users.noreply.github.com>
2020-05-10 13:37:56 +0100

    Create AssignmentOperator.py

---
+++ AssignmentOperator.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+# A program to Assignment Operators - Add, Subtract, Multiply, Divide, modules, exponent and floor division operations
+import math
+a=20
+b=10
+print ("Addition Operations of 2 assigned numbers {} and {} is {}".format(a,b,a+b))
+print ("Subtraction Operations of 2 assigned numbers {} and {} is {}".format(a,b,a-b))
+print ("Multiplication Operations of 2 assigned numbers {} and {} is {}".format(a,b,a*b))
+print ("Division Operations of 2 assigned numbers {} and {} is {}".format(a,b,a/b))
+print ("Modules Operations on assigned numbers {} and {} is {}".format(a,b,a%b))
+print ("Exponent Operations to power of 3 on assigned numbers {} and {} is {} {}".format(a,b,a**3,b**3))
+c=2.37
+print ("Floor of a assigned number {} is {}".format(c,math.floor(c)))
+print ("Ceil of a assigned number {} is {}".format(c,math.ceil(c)))
